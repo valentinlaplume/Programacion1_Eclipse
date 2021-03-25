f075cc15499032e1fae386345c4203b3fad328f8
valentinlaplume <laplu.me.valen@gmail.com>
2021-03-25 15:16:19 -0300

    tercer commit

---
+++ Practicando_clase_3/src/Practicando_clase_3.c
@@ -0,0 +1,39 @@
+/*
+ ============================================================================
+ Name        : Practicando_clase_3.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int funcionSumar (int numA,int numB,int* resul);
+
+int main(void)
+{
+	setbuf(stdout,NULL);
+	int a=2;
+	int b=3;
+	int resultado;
+
+	funcionSumar(a,b,&resultado);
+
+	printf("resultado: %d", resultado);
+
+
+
+	return EXIT_SUCCESS;
+}
+
+int funcionSumar (int numA,int numB,int* resul)
+{
+
+	*resul=numA + numB;
+
+
+	return 0;
+}
